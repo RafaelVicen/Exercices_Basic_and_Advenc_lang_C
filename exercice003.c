ab08f06465366eb4068099103f6ce6fbb5bfdb16
RafaelVicen <rafaelvic1946@gmail.com>
2024-01-09 00:16:36 +0100

    new commit

---
+++ exercice003.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+	3.efectua a soma dos numeros 5 e 10 e imprimir o resultado na tela.
+*/
+
+	int main()
+	{
+		int a, b, c;
+		
+			a = 5;
+		
+			b = 10;
+		
+			c= a + b;
+		
+		printf("valor da soma: %d", c);
+		
+		
+	}
