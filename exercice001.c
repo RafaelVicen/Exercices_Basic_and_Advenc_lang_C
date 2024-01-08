af78454bbe7ac1688e9c68335d64770d80985e0d
RafaelVicen <rafaelvic1946@gmail.com>
2024-01-09 00:01:50 +0100

    new commit

--- exercice001.c
+++ exercice001.c
@@ -1,12 +1,18 @@
-//1. Cria um programa pra ler e escrever a primeira letra do teu Nome.
 
 #include<stdio.h>
 #include<stdlib.h>
 
-	int main(){
+/*
+	1. Cria um programa pra ler e escrever a primeira letra do teu Nome.
+*/
+
+	int main()
+	{
 		char a;
 			printf("Digite a primeira letra do teu nome:");
+			
 				scanf("%c", &a);
+				
 			printf("A primeira letra do teu nome e : %c", a);
 		return 0;
 	}
---
+++ exercice002.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+	2. Cria um programa pra ler e escrever a sua idade.
+*/
+	
+	int main()
+	{
+		int idade;	
+			printf("Digite a sua idade: ");
+			
+				scanf("%d", &idade);
+				
+			printf("voce tem %d anos de idade.\n", idade);
+		return 0;
+	}
