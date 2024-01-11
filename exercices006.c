4d9f0e9366b3cbe39b0d7caeea69140cd1988927
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-10 22:22:20 -0300

    new commit

---
+++ exercices006.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/*
+6. Escreva um programa para ler um numero e escrever na tela o seu antecessor.
+ */
+int main()
+{
+    int n, a;
+	
+	printf("Digite um numero: ");
+			
+	    scanf("%d", &n);
+
+	    a=n-1
+				;
+	printf("O antecessor de %d eh %d",n,a);
+
+    return 0;
+	
+
+}
