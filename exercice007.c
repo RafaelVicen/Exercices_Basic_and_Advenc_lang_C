c11fe3f3b7c5f178fa7942e366d4cfb3501533bd
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-11 12:29:50 +0100

    new commit

---
+++ exercice007.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+/*
+  7.Escreva um programa para ler um numero e escrever na tela o seu sucessor.
+*/
+
+int main(){
+
+int n, s;
+    printf("Digite um numero: ");
+
+	scanf("%d",&n);
+
+	s=n + 1;
+
+    printf("o sucessor de %d eh %d",n,s);
+return 0;
+
+
+
+}
