c9150039261e38edf252064f70f2bccd991c9ef9
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-11 12:47:26 +0100

    new commit

---
+++ exercice008.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/*
+ 8. Cria um programa que leia duas notas de um aluno
+ e apresenta a media final
+ */
+int main(){
+
+  float  n1, n2, result;
+	 printf("Digite a primeira nota: ");
+
+	      scanf("%f",&n1);
+	 printf("Digite a segunda nota: ");
+
+	      scanf("%f",&n2);
+
+              result= n1 / n2;
+
+	 printf("A sua media final eh de %2f", result);
+ return 0;
+}
