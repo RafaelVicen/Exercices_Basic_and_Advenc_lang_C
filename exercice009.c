5ecda688e1406955e57a8483b29007f22359530b
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-12 17:17:08 +0100

    new commit

---
+++ exercice009.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/*
+   9. uma imobilaria vende terrenos retangulares
+   faça um programa para ler as dimensões de um terreno
+   e depois exebir a araea e comprimento
+ */
+  int main(){
+
+	  float  h, l, a, c;
+
+	 printf("Digite a altura");
+	      scanf("%f", &h);
+
+	printf("Digite a largura: ");
+             scanf("%f", &l);
+
+	     a = h * l;
+ 	     c = 2 * h + 2 * l;
+
+	printf("area = %f\n", a);
+	printf("comprimento = %f",c);
+
+return 0;
+  }
+
+
+
