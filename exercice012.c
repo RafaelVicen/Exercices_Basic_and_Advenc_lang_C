eef5169cc756a1527797463d26b18f7277e97cf2
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-14 10:08:52 +0100

    new commit

---
+++ exercice012.c
@@ -0,0 +1,28 @@
+/*
+ 
+   012) Ler uma temperatura em graus Celsius e apresentá-la
+   convertida em graus Fahrenheit.
+   A fórmula de conversão é: F = (9* C + 160) /5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
+
+    Entradas: C
+    Processamento: (9* C + 160) /5
+    Saída: F
+*/
+
+#include<stdio.h>
+int main(){
+	float c, f;
+
+
+	printf("Digite uma temperatura em graus Celsiu: ");
+	scanf("%f", &c);
+
+
+	f = (9 * c + 160) / 5;
+
+	printf("Em fahrenhei da %.2f ",  f);
+
+return 0;
+}
+
+
