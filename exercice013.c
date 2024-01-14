788ba1a2e4437441dc648d4d83847aa90fee4837
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-14 11:15:34 +0100

    new commit

--- exercice013.c
+++ exercice013.c
@@ -1,14 +1,28 @@
+/*
+  013 Tendo como dados de entrada a altura de  uma pessoa
+  contrua um programa q calcula um programa 
+  que calcule o peso ideial utilizando a formula
+  peso idea = (72.7 * h) - 58
+
+Entrada: altura
+processamento: (72.7 * h) - 58
+saida: pesoIdeal
+ */
+
+
 #include<stdio.h>
 int main(){
 	float altura, pesoIdeal;
-	char sexo;
+	
+	
+	printf("Digite a tua altura: ");
 
 	scanf("%f", &altura);
-	scanf("%c", &sexo);
+	
 
 	pesoIdeal = (72.7 * altura) - 58;
 
-	printf("%c %2.f", sexo, pesoIdeal);
+	printf(" %2.f", pesoIdeal);
 
 return 0;
 }
