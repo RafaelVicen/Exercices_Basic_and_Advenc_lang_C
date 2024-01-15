cb0e4fed262921f2a639b86987a271e6d0897c59
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-15 11:15:51 +0100

    mudanca no codigo

--- exercice014.c
+++ exercice014.c
@@ -8,18 +8,18 @@
 #include<stdio.h>
 	int main()
 {
-		char nome[] = "Rafael Vicente";
-		char sexo = 'M';
-		int idade = 35;
+		char nome[]="Rafael Vicente";
+		char sexo='M';
+		int idade=20;
 		float salario;
 
 		salario = 4250.90;
 
-		printf("Seus dados sao ");
-		printf("Nome: %s", nome );
-		printf("sexo %c", sexo  );
-		printf("voce tem %d anos", idade);
-		printf("voce ganha %f kwanza por mes", salario);
+		printf("Seus dados sao:\n");
+		printf("nome: %s\n",nome);
+		printf("sexo: %c\n",sexo);
+		printf("voce tem: %d anos\n",idade);
+		printf("voce ganha: %.2f kwanza por mes",salario);
 	 return 0;
 }	 
 
