d88447231201b8a1f6d379a6dcbaddf7d25b01e5
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-19 23:36:58 +0100

    frist commit

---
+++ exercice019.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+
+int i=i,n,soma=0;
+
+printf("Digite um numero: ");
+scanf("%d",&n);
+
+while(i <=n){
+	if(i%5==0);{
+		soma=soma+i;
+	}
+
+	i=i+1;
+
+}
+printf("%d",soma);
+
+
+
+
+
+return 0;
+
+
+}
