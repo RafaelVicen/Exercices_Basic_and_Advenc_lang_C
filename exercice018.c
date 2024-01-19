3bf59c92a4488745bfe00d4dabe73b527d4c1a7f
Rafaelvicen <rafaelvic1946@gmail.com>
2024-01-19 23:27:48 +0100

    frist commit

---
+++ exercice018.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+	int A,B,C;
+
+	printf("Digite o valor de A: ");
+	scanf("%i", &A);
+
+	printf("Digite o valor de B: ");
+	scanf("%i", &B);
+
+	C = A;
+	A = B;
+	B = C;
+
+	printf("A:%i, B:%i",A,B);
+
+
+
+
+
+
+
+	return 0;
+
+
+
+
+
+}
